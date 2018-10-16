12b2261da124b6d822cc61c9273707f082548d2b
Kevin <ds934118@gmail.com>
2018-10-16 23:38:49 +0800

    Finish fitness value

--- Chromosome.cpp
+++ Chromosome.cpp
@@ -7,14 +7,14 @@
 Chromosome::Chromosome(bool isRandom) {
     _fitnewssValue = 0;
     _wheelProbability = 0;
-    for(unsigned int i = 0; i < NumberOfDeterministicCustomers-1; i++) {
-        _timeWindows.emplace_back(TimeWindow(DefaultTimeWindow[i]));
+    for (int i : DefaultTimeWindow) {
+        _timeWindows.emplace_back(TimeWindow(i));
     }
     if(isRandom) random();
 
     for(unsigned int i = 0; i < NumberOfTimeWindowCase + 1; i++) numberOfTimeWindows.emplace_back(0);
-    for(unsigned int i = 0; i < _timeWindows.size(); i++) {
-        numberOfTimeWindows.at(_timeWindows.at(i).getCase())++;
+    for (auto &_timeWindow : _timeWindows) {
+        numberOfTimeWindows.at(static_cast<unsigned int>(_timeWindow.getCase()))++;
     }
 
     calculateFitnessValue();
@@ -28,42 +28,24 @@ Chromosome::Chromosome(std::vector<TimeWindow> &c) {
 }
 
 float Chromosome::calculateFitnessValue() {
-//    Car car[NumberOfVehicle];
-    float COST = 0;
-    float TW = 0;
-    int LOAD = 0;
-    int carCount = 0;
-    int nowCustomer, preCustomer;
-//    car[carCount].route.push_back(0);
-//    for(unsigned int i = 1; i < _customers.size(); i++) {
-//        LOAD += _customers.at(i).getDemandQuantity();
-//
-//        if(_customers.at(i).getID() >= NumberOfDeterministicCustomers) nowCustomer = _customers.at(i).getID() - 15;
-//        else nowCustomer = _customers.at(i).getID();
-//
-//        if(_customers.at(i-1).getID() >= NumberOfDeterministicCustomers) preCustomer = _customers.at(i-1).getID() - 15;
-//        else preCustomer = _customers.at(i-1).getID();
-//
-//        if(LOAD > CapacityOfVehicle) {
-//            COST += costMatrix[nowCustomer][0];
-//            car[++carCount].route.push_back(0);
-//            i--;
-//            LOAD = 0;
-//            continue;
-//        } else {
-//            TW = COST > _customers.at(i).getTimeWindow().getLowerBound() ? COST : _customers.at(i).getTimeWindow().getLowerBound();
-//            COST += costMatrix[preCustomer][nowCustomer] + _customers.at(i).getServiceTime();
-//            if (TW > _customers.at(i).getTimeWindow().getUpperBound())
-//                COST += penalty(TW, _customers.at(i).getTimeWindow().getUpperBound());
-//        }
-//    }
-    _fitnewssValue = COST;
-    return COST;
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < NumberOfDeterministicCustomers - 1; i++) {
+        sum1 += minimumCostForAssigningCustomer[i][_timeWindows.at(i).getCase() - 1];
+    }
+    sum1 *= Alpha;
+
+    for (int numberOfTimeWindow : numberOfTimeWindows) {
+        if ((numberOfTimeWindow - T) > 0)
+            sum2 += (numberOfTimeWindow - T);
+    }
+    sum2 *= Beta;
+    _fitnewssValue = sum1 + sum2;
+    return _fitnewssValue;
 }
 
 void Chromosome::random() {
     for(unsigned int i = 1; i < _timeWindows.size(); i++) {
-        int j = static_cast<int>((rand() % (NumberOfTimeWindowCase )) + 1);
+        int j = static_cast<int>((std::rand() % (NumberOfTimeWindowCase)) + 1);
         _timeWindows.at(i).setTimeWindowCase(j);
     }
     calculateFitnessValue();
@@ -72,12 +54,12 @@ void Chromosome::random() {
 void Chromosome::getTimeWindowCases() {
     for (auto &_timeWindow : _timeWindows)
         cout << _timeWindow.getCase() << " ";
-
+    cout << " Fitness Value : " << _fitnewssValue;
     cout << endl;
 }
 
 void Chromosome::getNumberOfTimeWindows() {
-    for(unsigned int i = 0; i < numberOfTimeWindows.size(); i++)
-        cout << numberOfTimeWindows.at(i) << " ";
+    for (int numberOfTimeWindow : numberOfTimeWindows)
+        cout << numberOfTimeWindow << " ";
     cout << endl << endl;
 }
